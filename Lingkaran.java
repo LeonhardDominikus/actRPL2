10e3887a83868d684be00b299208014e46249519
LeonhardDominikus <151561334+LeonhardDominikus@users.noreply.github.com>
2024-11-30 22:37:48 +0700

    Add files via upload

---
+++ Lingkaran.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Lingkaran {
+    public static void main(String[] args) {
+
+    int panjang = 20;
+    int lebar = 40;
+
+    int Luas = panjang * lebar;
+    int Keliling = 2 * panjang + 2 * lebar;
+
+    System.out.println("luas persegi = " + Luas);
+    System.out.println("luas keliling = " + Keliling);
+    }
+}
